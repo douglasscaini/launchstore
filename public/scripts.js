f6cfdfec2a689fc3143d3cfbe0a1b0c2e06109bd
Douglas Scaini <douglas.scaini@unoesc.edu.br>
2021-03-02 19:33:54 -0300

    photos uploud

--- public/scripts.js
+++ public/scripts.js
@@ -1,3 +1,4 @@
+// MÁSCARA DE VALOR DO PRODUCTS CREATE
 const Mask = {
   apply(input, func) {
     setTimeout(function () {
@@ -14,6 +15,111 @@ const Mask = {
   },
 };
 
+// LÓGICA DE INSERÇÃO DE FOTOS PRODUCT CREATE
+const PhotosUploud = {
+  input: "",
+  preview: document.querySelector("#photos-preview"),
+  uploudLimit: 6,
+  files: [],
+
+  handleFileInput(event) {
+    const { files: fileList } = event.target;
+
+    PhotosUploud.input = event.target;
+
+    if (PhotosUploud.hasLimit(event)) return;
+
+    Array.from(fileList).forEach((file) => {
+      PhotosUploud.files.push(file);
+
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        const image = new Image();
+
+        image.src = String(reader.result);
+
+        const div = PhotosUploud.getContainer(image);
+
+        PhotosUploud.preview.appendChild(div);
+      };
+
+      reader.readAsDataURL(file);
+    });
+
+    PhotosUploud.input.files = PhotosUploud.getAllFiles();
+  },
+  hasLimit(event) {
+    const { uploudLimit, input, preview } = PhotosUploud;
+    const { files: fileList } = input;
+
+    if (fileList.length > uploudLimit) {
+      alert(`Envie no máximo ${uploudLimit} fotos!`);
+      event.preventDefault();
+
+      return true;
+    }
+
+    const photosDiv = [];
+
+    preview.childNodes.forEach((item) => {
+      if (item.classList && item.classList.value == "photo") {
+        photosDiv.push(item);
+      }
+    });
+
+    const totalPhotos = fileList.length + photosDiv.length;
+
+    if (totalPhotos > uploudLimit) {
+      alert("Você atingiu o limite máximo de fotos!");
+      event.preventDefault();
+
+      return true;
+    }
+
+    return false;
+  },
+  getAllFiles() {
+    const dataTransfer = new ClipboardEvent("").clipboardData || new DataTransfer();
+
+    PhotosUploud.files.forEach((file) => dataTransfer.items.add(file));
+
+    return dataTransfer.files;
+  },
+  getContainer(image) {
+    const div = document.createElement("div");
+
+    div.classList.add("photo");
+
+    div.onclick = PhotosUploud.removePhoto;
+
+    div.appendChild(image);
+
+    div.appendChild(PhotosUploud.getRemoveButton());
+
+    return div;
+  },
+  getRemoveButton() {
+    const button = document.createElement("i");
+
+    button.classList.add("material-icons");
+    button.innerHTML = "close";
+
+    return button;
+  },
+  removePhoto(event) {
+    const photoDiv = event.target.parentNode;
+    const photosArray = Array.from(PhotosUploud.preview.children);
+    const index = photosArray.indexOf(photoDiv);
+
+    PhotosUploud.files.splice(index, 1);
+    PhotosUploud.input.files = PhotosUploud.getAllFiles();
+
+    photoDiv.remove();
+  },
+};
+
+// CONFIRMAÇÃO DE EXCLUSÃO PRODUCTS EDIT
 const formDelete = document.querySelector("#form-delete");
 if (formDelete) {
   itemFormDelete(formDelete);
@@ -22,6 +128,7 @@ if (formDelete) {
 function itemFormDelete(formDelete) {
   formDelete.addEventListener("submit", function (event) {
     const confirmation = confirm("Deseja Deletar?");
+
     if (!confirmation) {
       event.preventDefault();
     }
--- src/config/db.js
+++ src/config/db.js
@@ -1,9 +1,9 @@
 const { Pool } = require("pg");
 
 module.exports = new Pool({
-  user: "***",
-  password: "***",
-  host: "***",
+  user: "admin",
+  password: "admin",
+  host: "localhost",
   port: 5432,
   database: "launchstore",
 });
